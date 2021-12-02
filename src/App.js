9fcfe1abc44167f8956fa2bd822ba9b4a2d2bc8b
Dwayne Paisley-Marshall <dwayne.p-marshall@farfetch.com>
2021-12-02 19:30:22 +0100

    Added new packages and updated read me file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+function Array (){
+
+    const tet = ['Picnic','\n' , 'Basket']
+
+  return (
+     <h1>{tet}</h1>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Picnic basket</h1>
+      
+        <img src="https://picnic.app/nl/wp-content/uploads/sites/18/2020/11/logo.svg" className="App-logo" alt="logo" />
+        <Array />
       </header>
     </div>
   );

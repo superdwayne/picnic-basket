d15ba8e7e1013a71825241d81a8638ab2d742c2e
Dwayne Paisley-Marshall <dwayne.p-marshall@farfetch.com>
2021-12-03 11:53:57 +0100

    Saving work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
+import React from 'react';
 import './App.css';
 import { useMediaQuery } from 'react-responsive'
-
-
-function Array (){
-
-    const tet = ['Picnic','\n' , 'Basket']
-
-  return (
-     <h1>{tet}</h1>
-  )
+ 
+let Server;
+if (process.env.NODE_ENV === "production") {
+  Server = "https://picnic-basket.vercel.app/api";
+} else {
+  Server = "http://localhost:3000/";
 }
+
+
+// function Array (){
+
+//     const tet = ['Picnic','\n' , Math.round(Math.random() * 100)]
+
+//     const randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+//     console.log(randomColor)
+
+//     const xPostion = Math.round(Math.random() * 200)
+//     const yPostion = Math.round(Math.random() * 100)
+
+//     const element = React.createElement(
+//       'body', 
+//       { style: { background: '#'+randomColor, width: xPostion, height: yPostion  } },
+      
+//     )
+
+//   return (
+//      <h1>{element}</h1>
+//   )
+// } 
 
 
 
@@ -18,11 +39,15 @@ function App() {
   
   return (
     <div className="App">
-      <header className="App-header">
-      {isTabletOrMobile && <p>You are a desktop or laptop</p>}
-        <img src="https://picnic.app/nl/wp-content/uploads/sites/18/2020/11/logo.svg" className="App-logo" alt="logo" />
-        <Array />
-      </header>
+      <section className="App-header">
+
+      <h1>Why you should join</h1>
+
+      <form method="post" action={Server} encType="application/json">
+        <input type="text" id="fname" name="fname" placeholder="Join our waiting list" />
+        </form>
+       {isTabletOrMobile && <img src="http://dwaynep-marshall.co.uk/qrcode.png" className="qrcode" alt="QR code" /> }  
+      </section>
     </div>
   );
 }
